e61952f0b1182ab3b976000b861d40d97a9c48a3
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-09-07 23:00:06 -0300

    JSX 02

---
+++ exercicios/pages/basicos/jsx2.js
@@ -0,0 +1,15 @@
+export default function () {
+    const array = [
+        <li>Paulo</li>,
+        <li>Ana</li>,
+        <li>Maria</li>
+    ];
+
+    const conteudo = (
+        <div>
+            <h1>JSX #02</h1>
+        </div>
+    );
+
+    return array[2];
+}
