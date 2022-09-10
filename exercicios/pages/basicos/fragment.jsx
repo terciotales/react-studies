5fe3e68c7734de52c76840b01b43b71b7b820c2e
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-09-10 19:53:15 -0300

    Fragment

---
+++ exercicios/pages/basicos/fragment.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function () {
+    return (
+        <React.Fragment>
+            <h1>klahkljsha</h1>
+            <h2>asdasdasd</h2>
+        </React.Fragment>
+    )
+}
